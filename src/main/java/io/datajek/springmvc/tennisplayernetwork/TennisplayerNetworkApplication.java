90fd9d31570ab238f08ea3c9f0021260c4959479
Yakarakolev18 <yakarakolev18@codingburgas.bg>
2022-11-21 12:38:11 +0200

    Configure project

---
+++ src/main/java/io/datajek/springmvc/tennisplayernetwork/TennisplayerNetworkApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.springmvc.tennisplayernetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisplayerNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisplayerNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/datajek/springmvc/tennisplayernetwork/TennisplayerNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.springmvc.tennisplayernetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisplayerNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
